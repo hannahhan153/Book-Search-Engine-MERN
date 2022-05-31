2aae100e645dedcf6e4995b19322c9e01ff46048
hannahhan153 <hannah.han@biola.edu>
2022-05-31 11:33:22 -0700

    update resolvers on server with saveBook and removeBook and add mutations/queries to SearchBooks and SavedBooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+// import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -14,6 +18,7 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -65,6 +70,9 @@ const SearchBooks = () => {
     }
 
     try {
+      const mutationResponse = await saveBook({
+        variables: { bookData: {...bookToSave } },
+      });
       const response = await saveBook(bookToSave, token);
 
       if (!response.ok) {
