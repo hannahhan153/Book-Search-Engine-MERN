890619029d9cefcc40a16942c2edeadbd89c3162
hannahhan153 <hannah.han@biola.edu>
2022-05-30 18:00:59 -0700

    apollo provider for requests to work with apollo server

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/client';
-import { LOGIN } from '../utils/mutations';
+import { LOGIN_USER } from '../utils/mutations';
 // import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
@@ -11,7 +11,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [login, { error }] = useMutation(LOGIN);
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,13 +4,21 @@ const typeDefs = gql `
     type User {
         _id: ID
         username: String!
-        email: String
-        savedBooks: [Book]
+        email: String!
         bookCount: Int
+        savedBooks: [Book]
     }
 
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String
+    }
     type Auth {
-        token: ID
+        token: ID!
         user: User
     }
 
@@ -20,6 +28,9 @@ const typeDefs = gql `
 
     type Mutation {
         login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
     }
 `
 
